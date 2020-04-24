0bf6e1f7b1587b44e8a70c1218da7296a7beb034
JanhaviToshkhani <janhavitoshkhani94@gmail.com>
2020-04-24 14:17:45 +1000

    Added CSS minification to build script

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ProgressBarDemo from './components/ProgressBarDemo/ProgressBarDemo';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
-      <ProgressBarDemo/>
+    <BrowserRouter>
+      <Route path="/" exact component={ProgressBarDemo} />
+      <Redirect from="/:anything" to="/" />
+    </BrowserRouter>
   );
 }
 
